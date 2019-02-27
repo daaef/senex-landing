e5ff0ea3de1b468e04157172903f8e16d61e120a
cantell <rasheedmusa9@gmail.com>
2019-02-27 21:16:25 +0100

    enhancement: add footer to `simple` layout

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -2,15 +2,18 @@
   <div>
     <navbar theme="blue" logo-variant="blue" />
     <nuxt />
+    <footer-comp />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/navbar.vue'
+import Footer from '@/components/footer.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    FooterComp: Footer
   }
 }
 </script>
