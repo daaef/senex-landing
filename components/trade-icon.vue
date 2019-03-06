c7047e206cd91642124356d238530d34c4a39437
cantell <rasheedmusa9@gmail.com>
2019-03-06 12:34:09 +0100

    feat (components): add icons component

---
+++ components/trade-icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img :src="computedIconSrc" :alt="name">
+  </div>
+</template>
+
+<script>
+const ICONS_ROOT_DIR = '~assets/icons/'
+
+export default {
+  name: 'TradeIcon',
+  props: {
+    active: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    computedIconSrc() {
+      return this.$props.active
+        ? `${ICONS_ROOT_DIR}${this.$props.name}-active.png`
+        : `${ICONS_ROOT_DIR}${this.props.name}.png`
+    }
+  }
+}
+</script>
