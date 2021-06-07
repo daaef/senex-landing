1fa4f7291ccceb2d528b28060ee98d68a68f9c6e
Kolakanmi Apanisile <apanisilekolakanmi@gmail.com>
2021-06-07 02:35:46 +0100

    about page crypto-buddy

---
+++ components/about/CryptoBuddy.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="background-blue">
+      <div class="container">
+        <div class="word-con-abt">
+          <span>
+            <p class="cap-abt">SECURITY AND SMOOTHNESS</p>
+            <h2>The Perfect Crypto Buddy</h2>
+            <p class="lower-case-abt">
+              Candy lets you sell your airtime online in no time with just a
+              little discount. Get paid in your Candy wallet and send to your
+              bank account anytime you wish. Candy lets you sell your airtime
+              online in no time with just a little discount. Get paid in your C
+            </p>
+          </span>
+          <button type="button" class="register-btn-abt">
+            Start Trading Bitcoin
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
