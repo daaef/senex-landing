d592ef5a799c6c34034bfeac82aa79d6cb0f4c23
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2021-06-08 07:22:46 +0100

    - FAQ page search input - FAQ page list

---
+++ components/faq/FaqItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="faq-item-con">
+        <div class="faq-item-con__title-con">
+          <h3 class="title">{{ title }}</h3>
+          <span class="arrow-con" @click="toggleOpen"
+            ><img src="/img/awesome-arrow-down.svg"
+          /></span>
+        </div>
+        <div class="faq-item-con__content" :class="isOpen ? 'block' : 'hidden'">
+          {{ content }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleOpen() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/faq.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="faq-search-con">
+        <h3>Frequently Asked Questions.</h3>
+        <div class="search-con">
+          <span class="search-icon"><img src="/img/map-search.svg" /></span>
+          <input placeholder="Search for what you are looking for" />
+        </div>
+      </div>
+    </div>
+    <div class="faq-list-con">
+      <!-- FAQ List -->
+      <div class="faq-list">
+        <faq-item
+          title="Do I need to sign up to use Senex?"
+          content="Send professional invoices, track them in real time, and accept payments online—all from one place."
+        ></faq-item>
+      </div>
+      <!-- Check Help Center -->
+      <div class="container">
+        <span class="faq-help"
+          >Can’t find it here? Check our our
+          <router-link to="/help">Help Centre</router-link>.</span
+        >
+      </div>
+    </div>
+    <landing-footer></landing-footer>
+  </div>
+</template>
+
+<script>
+import FaqItem from '~/components/faq/FaqItem.vue'
+import LandingFooter from '~/components/LandingFooter.vue'
+export default {
+  components: { FaqItem, LandingFooter },
+  layout: 'about'
+}
+</script>
+
+<style></style>
