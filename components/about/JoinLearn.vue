8d88e22d49bcdf52b8a623e2cbe0625acfcc12aa
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2021-06-10 16:35:55 +0100

    landing footer socials

--- components/about/JoinLearn.vue
+++ components/about/JoinLearn.vue
@@ -2,39 +2,43 @@
   <div>
     <div class="container">
       <div class="lg:flex join-con">
-        <div class="join purple lg:mr-4">
-          <div class="diamond">
-            <img src="img/about/arrow-up-down.svg" alt="arrow" />
+        <div class="purple lg:mr-4">
+          <div class="join">
+            <div class="diamond">
+              <img src="img/about/arrow-up-down.svg" alt="arrow" />
+            </div>
+            <div class="">
+              <span>
+                <h2>Join our 'A' team</h2>
+                <p>
+                  Our mission is to establish a fully automated, fast, easy,
+                  decentralized, and secure digital payment
+                </p>
+              </span>
+            </div>
           </div>
-          <div class="words">
-            <span>
-              <h2>Join our 'A' team</h2>
-              <p>
-                Our mission is to establish a fully automated, fast, easy,
-                decentralized, and secure digital payment
-              </p>
-            </span>
-          </div>
-          <div class="arrow">
+          <span class="arrow">
             <img src="img/awesome-arrow-right.svg" alt="arrow" />
-          </div>
+          </span>
         </div>
-        <div class="join green">
-          <div class="diamond">
-            <img src="img/about/green-badge.svg" alt="badge" />
+        <div class="green lg:mr-4">
+          <div class="join">
+            <div class="diamond">
+              <img src="img/about/green-badge.svg" alt="arrow" />
+            </div>
+            <div class="">
+              <span>
+                <h2>Learn with Senex</h2>
+                <p>
+                  Our mission is to establish a fully automated, fast, easy,
+                  decentralized, and secure digital payment
+                </p>
+              </span>
+            </div>
           </div>
-          <div class="words">
-            <span>
-              <h2>Learn with Senex</h2>
-              <p>
-                Our mission is to establish a fully automated, fast, easy,
-                decentralized, and secure digital payment
-              </p>
-            </span>
-          </div>
-          <div class="arrow">
+          <span class="arrow">
             <img src="img/awesome-arrow-right.svg" alt="arrow" />
-          </div>
+          </span>
         </div>
       </div>
     </div>
