3165f50159cab2b9b13b69546ef319816d78d37a
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2021-06-23 18:41:56 +0100

    Careers page

--- components/home/Review.vue
+++ components/home/Review.vue
@@ -2,21 +2,28 @@
   <div>
     <div class="container lnd-rvw-con">
       <div class="lnd-review md:flex">
-        <span class="img-con w-full md:w-1/3"
-          ><img src="/img/landing-page-review.png"
-        /></span>
+        <transition name="slide-fade" mode="out-in">
+          <span :key="current" class="img-con w-full md:w-1/3"
+            ><img :src="reviews[current].image"
+          /></span>
+        </transition>
         <div class="rvw-con md:w-2/3">
-          <span class="rvw-text"
-            >Senexpay is the most reliable and fastest Bitcoin exchanger I have
-            ever used. It’s so easy I was wondering why I have not been using it
-            since.</span
-          >
-          <h3 class="rvw-writer">Akinosho, from Lagos</h3>
+          <transition name="slide-fade" mode="out-in">
+            <div :key="current" class="rvw-text-con">
+              <span class="rvw-text">{{ reviews[current].text }}</span>
+              <h3 class="rvw-writer">{{ reviews[current].writer }}</h3>
+            </div>
+          </transition>
           <span class="rvw-con__arrow-con md:w-1/6 md:ml-auto"
-            ><img src="/img/awesome-arrow-left.svg" class="left-anim" />
+            ><img
+              src="/img/awesome-arrow-left.svg"
+              class="left-anim"
+              @click="changeIndex(-1)"
+            />
             <img
               src="/img/awesome-arrow-right.svg"
               class="right-img right-anim"
+              @click="changeIndex(1)"
             />
           </span>
         </div>
@@ -26,7 +33,50 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      reviews: [
+        {
+          image: '/img/landing-page-review.png',
+          text: 'Senexpay is the most reliable and fastest Bitcoin exchanger I have ever used. It’s so easy I was wondering why I have not been using it since.',
+          writer: 'Akinosho, from Lagos'
+        },
+        {
+          image: '/img/about/ceo.png',
+          text: 'Senexpay is the most reliable and fastest.',
+          writer: 'CEO, from Lagos'
+        }
+      ],
+      current: 0
+    }
+  },
+  methods: {
+    changeIndex(n) {
+      const newIdx = this.current + n
+      // this.current = newIdx
+      if (newIdx < this.reviews.length && newIdx >= 0) {
+        this.current = newIdx
+      } else if (newIdx < 0) {
+        this.current = this.reviews.length - 1
+      } else {
+        this.current = 0
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
---
+++ components/home/ReviewHolder.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      reviews: [],
+      current: 0
+    }
+  },
+  methods: {
+    changeIndex(n) {
+      const newIdx = this.current + n
+      if (newIdx < this.reviews.length) {
+        this.current = newIdx >= 0 ? newIdx : 0
+      } else {
+        this.current = 0
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
