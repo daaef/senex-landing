8c60f81f1f59e402b69d2d97adc8bcaf4614d51b
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2021-07-06 19:38:49 +0100

    secure svg transition

--- pages/index.vue
+++ pages/index.vue
@@ -251,8 +251,157 @@
           </div>
         </div>
         <div class="lg:w-1/2 flex">
-          <div class="lg:w-1/2 m-auto snx-description-img-con">
-            <img src="/img/secure.svg" />
+          <div class="m-auto snx-description-img-con">
+            <!-- <img src="/img/secure.svg" /> -->
+            <svg
+              id="Component_109_1"
+              class="secure-lock-svg"
+              data-name="Component 109 – 1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="495"
+              height="484"
+              viewBox="0 0 495 484"
+            >
+              <rect
+                id="Rectangle_830"
+                data-name="Rectangle 830"
+                width="441"
+                height="441"
+                rx="30"
+                transform="translate(54)"
+                fill="#e4edfc"
+              />
+              <rect
+                id="Rectangle_831"
+                data-name="Rectangle 831"
+                width="441"
+                height="441"
+                rx="30"
+                transform="translate(0 43)"
+                fill="#ddffeb"
+              />
+              <g
+                id="Rectangle_829"
+                data-name="Rectangle 829"
+                transform="translate(28 18)"
+                fill="#fff"
+                stroke="#1370f8"
+                stroke-width="4"
+              >
+                <rect width="441" height="441" rx="30" stroke="none" />
+                <rect
+                  x="2"
+                  y="2"
+                  width="437"
+                  height="437"
+                  rx="28"
+                  fill="none"
+                />
+              </g>
+              <g
+                id="Ellipse_117"
+                data-name="Ellipse 117"
+                transform="translate(190 180)"
+                fill="none"
+                stroke="#fff"
+                stroke-width="4"
+              >
+                <circle cx="59" cy="59" r="59" stroke="none" />
+                <circle cx="59" cy="59" r="57" fill="none" />
+              </g>
+              <g
+                id="Group_694"
+                data-name="Group 694"
+                transform="translate(-1096 -4024)"
+              >
+                <rect
+                  id="diagonal-1"
+                  data-name="Rectangle 832"
+                  width="7"
+                  height="589"
+                  rx="3.5"
+                  transform="translate(1131.154 4059.3) rotate(-45)"
+                  fill="#e4edfc"
+                />
+                <rect
+                  id="diagonal-2"
+                  data-name="Rectangle 833"
+                  width="7"
+                  height="591"
+                  rx="3.5"
+                  transform="translate(1137.518 4473.664) rotate(-135)"
+                  fill="#e4edfc"
+                />
+              </g>
+              <circle
+                id="blue-circle-svg"
+                data-name="blue-circle-svg"
+                cx="50%"
+                cy="50%"
+                r="122"
+                fill="#1370f8"
+              />
+              <g
+                id="Group_698"
+                data-name="Group 698"
+                transform="translate(-1096 -4014)"
+              >
+                <g
+                  id="shield_1_"
+                  data-name="shield (1)"
+                  transform="translate(1240.11 4179)"
+                >
+                  <path
+                    id="Path_2064"
+                    data-name="Path 2064"
+                    d="M104.989,0,30.912,21.137V58.274a118.967,118.967,0,0,0,72.1,109.436l1.975.791,1.975-.791a118.967,118.967,0,0,0,72.1-109.436V21.137Z"
+                    fill="#e7e7e7"
+                  />
+                  <path
+                    id="Path_2065"
+                    data-name="Path 2065"
+                    d="M330.077,21.137V58.274a118.967,118.967,0,0,1-72.1,109.436L256,168.5V0Z"
+                    transform="translate(-151.011)"
+                    fill="#d3d3d8"
+                  />
+                  <path
+                    id="Path_2066"
+                    data-name="Path 2066"
+                    d="M145.259,62.425,90.936,77.932v22.223a99.244,99.244,0,0,0,51.953,87.312l2.371,1.284,2.371-1.284a99.243,99.243,0,0,0,51.953-87.312V77.932Z"
+                    transform="translate(-40.27 -41.881)"
+                    fill="#00abe9"
+                  />
+                  <path
+                    id="Path_2067"
+                    data-name="Path 2067"
+                    d="M310.323,77.932v22.223a99.244,99.244,0,0,1-51.953,87.312L256,188.75V62.425Z"
+                    transform="translate(-151.011 -41.881)"
+                    fill="#0095ff"
+                  />
+                </g>
+                <g
+                  id="Path_group"
+                  data-name="Group 695"
+                  transform="translate(13.5 13.5)"
+                >
+                  <circle
+                    id="Ellipse_118"
+                    data-name="Ellipse 118"
+                    cx="33"
+                    cy="33"
+                    r="33"
+                    transform="translate(1298.5 4216.5)"
+                    fill="#fff"
+                  />
+                  <path
+                    id="ios-lock"
+                    data-name="Icon ionic-ios-lock"
+                    d="M30.294,15.483H28.276V11.557a8.073,8.073,0,1,0-16.144-.109v4.036H10.113A3.373,3.373,0,0,0,6.75,18.847V34.991a3.373,3.373,0,0,0,3.363,3.363h20.18a3.373,3.373,0,0,0,3.363-3.363V18.847A3.373,3.373,0,0,0,30.294,15.483ZM21.381,25.977v5.785a1.206,1.206,0,0,1-1.118,1.211A1.179,1.179,0,0,1,19.026,31.8V25.977a2.691,2.691,0,1,1,2.354,0Zm4.541-10.494H14.486V11.447a5.718,5.718,0,0,1,11.436,0Z"
+                    fill="#1370f8"
+                  />
+                </g>
+              </g>
+            </svg>
           </div>
         </div>
       </div>
