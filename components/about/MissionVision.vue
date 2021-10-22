3f3c52401b683fb8600ddd7c267c4de615ff06f4
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2021-10-22 19:26:13 +0100

    about foreword

--- components/about/MissionVision.vue
+++ components/about/MissionVision.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="container">
     <div class="mission-container">
-      <div class="sm:flex justify-center">
-        <div class="our-mission-con sm:mr-12">
-          <div class="double-arrow">
-            <img src="img/about/double-right-arrow.svg" alt="skip" />
-          </div>
-          <span>
-            <h2 class="heading-tertiary">Our Mission</h2>
-            <p class="paragraph">
-              Our mission is to be the brand of choice in the digital financial
-              ecosystem and be a major driver of modern financial innovation in
-              Africa.
-            </p>
-          </span>
+      <!-- <div class="sm:flex justify-center align-items-center"> -->
+      <div class="our-mission-con sm:mr-12">
+        <div class="double-arrow">
+          <img src="img/about/double-right-arrow.svg" alt="skip" />
         </div>
-        <div class="our-mission-con">
-          <div class="double-arrow">
-            <img src="img/about/green-eye.svg" alt="eye" />
-          </div>
-          <span>
-            <h2 class="heading-tertiary">Our Vision</h2>
-            <p class="paragraph">
-              Our vision is to make financial services accessible to every
-              person on the African continent. We believe empowering people will
-              boundless inclusion.
-            </p>
-          </span>
-        </div>
+        <span>
+          <h2 class="heading-tertiary">Our Mission</h2>
+          <p class="paragraph">
+            Our mission is to be the brand of choice in the digital financial
+            ecosystem and be a major driver of modern financial innovation in
+            Africa.
+          </p>
+        </span>
       </div>
+      <div class="our-mission-con">
+        <div class="double-arrow">
+          <img src="img/about/green-eye.svg" alt="eye" />
+        </div>
+        <span>
+          <h2 class="heading-tertiary">Our Vision</h2>
+          <p class="paragraph">
+            Our vision is to make financial services accessible to every person
+            on the African continent. We believe empowering people will
+            boundless inclusion.
+          </p>
+        </span>
+      </div>
+      <!-- </div> -->
     </div>
   </div>
 </template>
