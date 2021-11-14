20fe2ea10c8452b894f7e8e4f76703b9fbbcae57
Chibuotu Amadi <chibuotu@gmail.com>
2021-11-14 04:14:35 +0000

    pm2 port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'v3_client',
-      port: 3007,
+      port: 3003,
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
