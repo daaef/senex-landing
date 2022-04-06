262205c390247aeab6336b45aadcb4e43c99e32c
Apanisileik <kolakanmi.apanisile@sterling.ng>
2022-04-06 06:40:53 +0100

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'v3_client',
-      port: 3003,
+      port: 3007,
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
