4c4fa1cd27e0165109205627cdd5ac8b03befc0f
Afekhide Gbadamosi <realafe21@gmail.com>
2022-09-28 01:54:03 +0100

    updated schema in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,52 +17,30 @@ export default {
       },
       {
         json: {
-                '@context': 'https://schema.org',
-                '@type': 'Organization',
-                name: 'SenexPay',
-                alternateName: 'Senex',
-                url: 'https://www.senexpay.com',
-                logo: 'https://www.senexpay.com/img/senexpay-logo-light.svg',
-                address: {
-                  '@type': 'PostalAddress',
-                  addressLocality: 'Lagos, Nigeria',
-                  streetAddress:
-                    'Number One Lagos, 1, Akin Adesola Street, Victoria Island.'
-                },
-                email: 'hello@senexpay.com',
-                telephone: '+234-916-259-8206',
-                description:
-                  "SenexPay is the easiest and most reliable platform to exchange cryptocurrency. We are building Africa's biggest cross-border payments infrastructure. Pay seamlessly with Senex today!",
-                sameAs: [
-                  'https://web.facebook.com/senexpay/',
-                  'https://twitter.com/senexpay/',
-                  'https://www.youtube.com/channel/UCf-zuXt2vynE1TFeiY1sw1Q',
-                  'https://www.instagram.com/senexpay/',
-                  'https://linkedin.com/company/senexpay/'
-                ],
-                potentialAction: {
-                  '@type': 'ViewAction',
-                  target: [
-                    'https://www.senexpay.com',
-                    {
-                      '@type': 'EntryPoint',
-                      urlTemplate: 'https://senexpay.com',
-                      contentType: 'application/json+ld'
-                    },
-                    'android-app://com.senexpay.mobile',
-                    {
-                      '@type': 'EntryPoint',
-                      urlTemplate: 'com.senexpay.mobile',
-                      application: {
-                        '@type': 'SoftwareApplication',
-                        '@id': '5MPM3W735H',
-                        name: 'Senexpay iPhone and iPad App',
-                        operatingSystem: 'iOS'
-                      }
-                    }
-                  ]
-                }
-              },
+          '@context': 'https://schema.org',
+          '@type': 'Organization',
+          name: 'SenexPay',
+          alternateName: 'Senex Payment Services',
+          url: 'https://www.senexpay.com',
+          logo: 'https://www.senexpay.com/senexpay-logo.jpeg',
+          address: {
+            '@type': 'PostalAddress',
+            addressLocality: 'Lagos, Nigeria',
+            streetAddress:
+              'Number One Lagos, 1, Akin Adesola Street, Victoria Island.'
+          },
+          email: 'hello@senexpay.com',
+          telephone: '+234-916-259-8206',
+          description:
+            "SenexPay is the easiest and most reliable platform to exchange cryptocurrency. We are building Africa's biggest cross-border payments infrastructure. Pay seamlessly with Senex today!",
+          sameAs: [
+            'https://web.facebook.com/senexpay/',
+            'https://twitter.com/senexpay/',
+            'https://www.youtube.com/channel/UCf-zuXt2vynE1TFeiY1sw1Q',
+            'https://www.instagram.com/senexpay/',
+            'https://linkedin.com/company/senexpay/'
+          ]
+        },
         type: 'application/ld+json'
       }
     ],
@@ -146,11 +124,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    // '~/plugins/jsonld',
     { src: '~/plugins/vue-scroll-reveal', ssr: false },
     { src: '~/plugins/axios', ssr: true },
     { src: '~plugins/vue-typer', mode: 'client' },
-    { src: '~/plugins/vue-carousel', ssr: false },
+    { src: '~/plugins/vue-carousel', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
